fa1492b943951e2f9c61de91a51b19b675c0bd16
Mathew Kluczyk <mathew.klucznyk@gmail.com>
2020-09-18 11:31:28 -0400

    Added latching and pulse save/recall modes to the Snapshot Controller

--- QscQsys/Production/SPlsWork/Qsys_Snapshot_Controller.cs
+++ QscQsys/Production/SPlsWork/Qsys_Snapshot_Controller.cs
@@ -23,6 +23,7 @@ namespace UserModule_QSYS_SNAPSHOT_CONTROLLER
         Crestron.Logos.SplusObjects.AnalogInput NUMBER;
         StringParameter COREID;
         StringParameter COMPONENTNAME;
+        UShortParameter MODE;
         QscQsys.QsysSnapshot SNAPSHOT;
         object NUMBER_OnChange_0 ( Object __EventInfo__ )
         
@@ -34,30 +35,35 @@ namespace UserModule_QSYS_SNAPSHOT_CONTROLLER
                 ushort X = 0;
                 
                 
-                __context__.SourceCodeLine = 17;
-                while ( Functions.TestForTrue  ( ( Functions.BoolToInt (X != NUMBER  .UshortValue))  ) ) 
+                __context__.SourceCodeLine = 22;
+                if ( Functions.TestForTrue  ( ( Functions.BoolToInt (MODE  .Value == 1))  ) ) 
                     { 
-                    __context__.SourceCodeLine = 19;
-                    X = (ushort) ( NUMBER  .UshortValue ) ; 
-                    __context__.SourceCodeLine = 21;
-                    if ( Functions.TestForTrue  ( ( LOAD  .Value)  ) ) 
+                    __context__.SourceCodeLine = 24;
+                    while ( Functions.TestForTrue  ( ( Functions.BoolToInt (X != NUMBER  .UshortValue))  ) ) 
                         { 
-                        __context__.SourceCodeLine = 23;
-                        SNAPSHOT . LoadSnapshot ( (ushort)( X )) ; 
-                        } 
-                    
-                    else 
-                        {
-                        __context__.SourceCodeLine = 25;
-                        if ( Functions.TestForTrue  ( ( SAVE  .Value)  ) ) 
+                        __context__.SourceCodeLine = 26;
+                        X = (ushort) ( NUMBER  .UshortValue ) ; 
+                        __context__.SourceCodeLine = 28;
+                        if ( Functions.TestForTrue  ( ( LOAD  .Value)  ) ) 
                             { 
-                            __context__.SourceCodeLine = 27;
-                            SNAPSHOT . SaveSnapshot ( (ushort)( X )) ; 
+                            __context__.SourceCodeLine = 30;
+                            SNAPSHOT . LoadSnapshot ( (ushort)( X )) ; 
                             } 
                         
-                        }
+                        else 
+                            {
+                            __context__.SourceCodeLine = 32;
+                            if ( Functions.TestForTrue  ( ( SAVE  .Value)  ) ) 
+                                { 
+                                __context__.SourceCodeLine = 34;
+                                SNAPSHOT . SaveSnapshot ( (ushort)( X )) ; 
+                                } 
+                            
+                            }
+                        
+                        __context__.SourceCodeLine = 24;
+                        } 
                     
-                    __context__.SourceCodeLine = 17;
                     } 
                 
                 
@@ -69,98 +75,150 @@ namespace UserModule_QSYS_SNAPSHOT_CONTROLLER
             
         }
         
-    public override object FunctionMain (  object __obj__ ) 
+    object LOAD_OnPush_1 ( Object __EventInfo__ )
+    
         { 
+        Crestron.Logos.SplusObjects.SignalEventArgs __SignalEventArg__ = (Crestron.Logos.SplusObjects.SignalEventArgs)__EventInfo__;
         try
         {
-            SplusExecutionContext __context__ = SplusFunctionMainStartCode();
+            SplusExecutionContext __context__ = SplusThreadStartCode(__SignalEventArg__);
             
-            __context__.SourceCodeLine = 34;
-            SNAPSHOT . Initialize ( COREID  .ToString(), COMPONENTNAME  .ToString()) ; 
+            __context__.SourceCodeLine = 42;
+            if ( Functions.TestForTrue  ( ( Functions.BoolToInt (MODE  .Value == 2))  ) ) 
+                { 
+                __context__.SourceCodeLine = 44;
+                SNAPSHOT . LoadSnapshot ( (ushort)( NUMBER  .UshortValue )) ; 
+                } 
+            
             
             
         }
         catch(Exception e) { ObjectCatchHandler(e); }
-        finally { ObjectFinallyHandler(); }
-        return __obj__;
-        }
-        
-    
-    public override void LogosSplusInitialize()
-    {
-        SocketInfo __socketinfo__ = new SocketInfo( 1, this );
-        InitialParametersClass.ResolveHostName = __socketinfo__.ResolveHostName;
-        _SplusNVRAM = new SplusNVRAM( this );
-        
-        LOAD = new Crestron.Logos.SplusObjects.DigitalInput( LOAD__DigitalInput__, this );
-        m_DigitalInputList.Add( LOAD__DigitalInput__, LOAD );
-        
-        SAVE = new Crestron.Logos.SplusObjects.DigitalInput( SAVE__DigitalInput__, this );
-        m_DigitalInputList.Add( SAVE__DigitalInput__, SAVE );
-        
-        NUMBER = new Crestron.Logos.SplusObjects.AnalogInput( NUMBER__AnalogSerialInput__, this );
-        m_AnalogInputList.Add( NUMBER__AnalogSerialInput__, NUMBER );
-        
-        COREID = new StringParameter( COREID__Parameter__, this );
-        m_ParameterList.Add( COREID__Parameter__, COREID );
-        
-        COMPONENTNAME = new StringParameter( COMPONENTNAME__Parameter__, this );
-        m_ParameterList.Add( COMPONENTNAME__Parameter__, COMPONENTNAME );
-        
-        
-        NUMBER.OnAnalogChange.Add( new InputChangeHandlerWrapper( NUMBER_OnChange_0, true ) );
-        
-        _SplusNVRAM.PopulateCustomAttributeList( true );
-        
-        NVRAM = _SplusNVRAM;
+        finally { ObjectFinallyHandler( __SignalEventArg__ ); }
+        return this;
         
     }
     
-    public override void LogosSimplSharpInitialize()
+object SAVE_OnPush_2 ( Object __EventInfo__ )
+
+    { 
+    Crestron.Logos.SplusObjects.SignalEventArgs __SignalEventArg__ = (Crestron.Logos.SplusObjects.SignalEventArgs)__EventInfo__;
+    try
     {
-        SNAPSHOT  = new QscQsys.QsysSnapshot();
+        SplusExecutionContext __context__ = SplusThreadStartCode(__SignalEventArg__);
         
+        __context__.SourceCodeLine = 50;
+        if ( Functions.TestForTrue  ( ( Functions.BoolToInt (MODE  .Value == 2))  ) ) 
+            { 
+            __context__.SourceCodeLine = 52;
+            SNAPSHOT . SaveSnapshot ( (ushort)( NUMBER  .UshortValue )) ; 
+            } 
         
-    }
-    
-    public UserModuleClass_QSYS_SNAPSHOT_CONTROLLER ( string InstanceName, string ReferenceID, Crestron.Logos.SplusObjects.CrestronStringEncoding nEncodingType ) : base( InstanceName, ReferenceID, nEncodingType ) {}
-    
-    
-    
-    
-    const uint LOAD__DigitalInput__ = 0;
-    const uint SAVE__DigitalInput__ = 1;
-    const uint NUMBER__AnalogSerialInput__ = 0;
-    const uint COREID__Parameter__ = 10;
-    const uint COMPONENTNAME__Parameter__ = 11;
-    
-    [SplusStructAttribute(-1, true, false)]
-    public class SplusNVRAM : SplusStructureBase
-    {
-    
-        public SplusNVRAM( SplusObject __caller__ ) : base( __caller__ ) {}
         
         
     }
+    catch(Exception e) { ObjectCatchHandler(e); }
+    finally { ObjectFinallyHandler( __SignalEventArg__ ); }
+    return this;
     
-    SplusNVRAM _SplusNVRAM = null;
+}
+
+public override object FunctionMain (  object __obj__ ) 
+    { 
+    try
+    {
+        SplusExecutionContext __context__ = SplusFunctionMainStartCode();
+        
+        __context__.SourceCodeLine = 58;
+        SNAPSHOT . Initialize ( COREID  .ToString(), COMPONENTNAME  .ToString()) ; 
+        
+        
+    }
+    catch(Exception e) { ObjectCatchHandler(e); }
+    finally { ObjectFinallyHandler(); }
+    return __obj__;
+    }
     
-    public class __CEvent__ : CEvent
-    {
-        public __CEvent__() {}
-        public void Close() { base.Close(); }
-        public int Reset() { return base.Reset() ? 1 : 0; }
-        public int Set() { return base.Set() ? 1 : 0; }
-        public int Wait( int timeOutInMs ) { return base.Wait( timeOutInMs ) ? 1 : 0; }
-    }
-    public class __CMutex__ : CMutex
-    {
-        public __CMutex__() {}
-        public void Close() { base.Close(); }
-        public void ReleaseMutex() { base.ReleaseMutex(); }
-        public int WaitForMutex() { return base.WaitForMutex() ? 1 : 0; }
-    }
-     public int IsNull( object obj ){ return (obj == null) ? 1 : 0; }
+
+public override void LogosSplusInitialize()
+{
+    _SplusNVRAM = new SplusNVRAM( this );
+    
+    LOAD = new Crestron.Logos.SplusObjects.DigitalInput( LOAD__DigitalInput__, this );
+    m_DigitalInputList.Add( LOAD__DigitalInput__, LOAD );
+    
+    SAVE = new Crestron.Logos.SplusObjects.DigitalInput( SAVE__DigitalInput__, this );
+    m_DigitalInputList.Add( SAVE__DigitalInput__, SAVE );
+    
+    NUMBER = new Crestron.Logos.SplusObjects.AnalogInput( NUMBER__AnalogSerialInput__, this );
+    m_AnalogInputList.Add( NUMBER__AnalogSerialInput__, NUMBER );
+    
+    MODE = new UShortParameter( MODE__Parameter__, this );
+    m_ParameterList.Add( MODE__Parameter__, MODE );
+    
+    COREID = new StringParameter( COREID__Parameter__, this );
+    m_ParameterList.Add( COREID__Parameter__, COREID );
+    
+    COMPONENTNAME = new StringParameter( COMPONENTNAME__Parameter__, this );
+    m_ParameterList.Add( COMPONENTNAME__Parameter__, COMPONENTNAME );
+    
+    
+    NUMBER.OnAnalogChange.Add( new InputChangeHandlerWrapper( NUMBER_OnChange_0, true ) );
+    LOAD.OnDigitalPush.Add( new InputChangeHandlerWrapper( LOAD_OnPush_1, false ) );
+    SAVE.OnDigitalPush.Add( new InputChangeHandlerWrapper( SAVE_OnPush_2, false ) );
+    
+    _SplusNVRAM.PopulateCustomAttributeList( true );
+    
+    NVRAM = _SplusNVRAM;
+    
+}
+
+public override void LogosSimplSharpInitialize()
+{
+    SNAPSHOT  = new QscQsys.QsysSnapshot();
+    
+    
+}
+
+public UserModuleClass_QSYS_SNAPSHOT_CONTROLLER ( string InstanceName, string ReferenceID, Crestron.Logos.SplusObjects.CrestronStringEncoding nEncodingType ) : base( InstanceName, ReferenceID, nEncodingType ) {}
+
+
+
+
+const uint LOAD__DigitalInput__ = 0;
+const uint SAVE__DigitalInput__ = 1;
+const uint NUMBER__AnalogSerialInput__ = 0;
+const uint COREID__Parameter__ = 10;
+const uint COMPONENTNAME__Parameter__ = 11;
+const uint MODE__Parameter__ = 12;
+
+[SplusStructAttribute(-1, true, false)]
+public class SplusNVRAM : SplusStructureBase
+{
+
+    public SplusNVRAM( SplusObject __caller__ ) : base( __caller__ ) {}
+    
+    
+}
+
+SplusNVRAM _SplusNVRAM = null;
+
+public class __CEvent__ : CEvent
+{
+    public __CEvent__() {}
+    public void Close() { base.Close(); }
+    public int Reset() { return base.Reset() ? 1 : 0; }
+    public int Set() { return base.Set() ? 1 : 0; }
+    public int Wait( int timeOutInMs ) { return base.Wait( timeOutInMs ) ? 1 : 0; }
+}
+public class __CMutex__ : CMutex
+{
+    public __CMutex__() {}
+    public void Close() { base.Close(); }
+    public void ReleaseMutex() { base.ReleaseMutex(); }
+    public int WaitForMutex() { return base.WaitForMutex() ? 1 : 0; }
+}
+ public int IsNull( object obj ){ return (obj == null) ? 1 : 0; }
 }
 
 
